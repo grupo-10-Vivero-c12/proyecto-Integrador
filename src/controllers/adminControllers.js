a3cec8cc417939bd24f44fd8ec777411f8d46790
Exequiel65 <marcos97britos@gmail.com>
2021-11-28 20:48:36 -0300

    correccion json

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,4 +1,4 @@
-let {getProductos} = require('../data/dateBase')
+// let {getProductos} = require('../data/dateBase')
 
 let controller = {
     home:(req, res) =>{
--- src/data/dateBase.js
+++ src/data/dateBase.js
@@ -1,10 +1,11 @@
-let fs = require('fs');
+// let fs = require('fs');
 
-module.exports = {
-    getSucursales : JSON.parse(fs.readFileSync('./data/products.json', 'utf-8')),
-    writeJson : (dataBase) => {
-        fs.writeFileSync('./data/concesionarias.json', JSON.stringify(dataBase), "utf-8")
-    },
 
-}
+// module.exports = {
+//     getProductos : JSON.parse(fs.readFileSync('./products.json', 'utf-8')),
+//     writeJson : (dataBase) => {
+//         fs.writeFileSync('./data/concesionarias.json', JSON.stringify(dataBase), "utf-8")
+//     },
+
+// }
 
