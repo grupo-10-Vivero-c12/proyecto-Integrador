f6ae7704acef035c8de076238bc6c13181d5d74f
Exequiel65 <marcos97britos@gmail.com>
2022-01-07 20:38:39 -0300

    fix

--- src/middlewares/cookieSession.js
+++ src/middlewares/cookieSession.js
@@ -1,5 +1,5 @@
 function cookieSession (req, res, next) {
-    if(req.cookies.userTimbo){
+    if (req.cookies.userTimbo) {
         req.session.user = req.cookies.userTimbo;
         res.locals.user = req.session.user
     }
