f5ce2b0e14ccf90fa9f3892cac72baa2985bb7bd
DanielabGuardia <danielabguardia@gmail.com>
2022-03-23 16:46:53 -0300

    arreglos

--- public/js/login.js
+++ public/js/login.js
@@ -1,64 +1,26 @@
- window.onload = ()=>{
-        let $inputPass = document.querySelector('#password')
+
+var email = document.getElementById('#email');
+var password = document.getElementById('#password');
+var button = document.querySelector('iniciar');
+
+
+button.addEventListener('blur', function(event) {
+    event.preventDefault();
+        if(password.value === "" ){
+            console.log('El campo contraseña es obligatorio')
+           
+            return false
+          
+        }else if(email.value === ""){
+            console.log('El campo email es obligatorio')
+            return false;
+
+        }else if(email.value.length > 40){
+            console.log('El email es demasiado largo')
+            return
+        }
+       
+    })
+
+    alert('conectado')
     
-        let $inputs = document.querySelectorAll('.input-js')
-        let $button = document.querySelector('#buttonSubmit')
-        let changePassowrd = document.querySelector('#change-password')
-        let $btnH5 = document.querySelector('#btn-change-password')
-        let updateFile = document.querySelector('#file')
-        let btnSubmitFile = document.querySelector('#submitFile')
-        
-        btnSubmitFile.style.display = "none"
-    
-        for (let i = 0; i < $inputs.length; i++) {
-            if ($inputs[i].classList.contains('is-invalid')) {
-                for (let i = 0; i < $inputs.length; i++) {
-                    $editar.style.display = "none"
-                    $inputs[i].disabled = false
-                    $inputs[i].style.color = "black"
-                    $button.style.display = "block"
-                } 
-                break
-            } 
-        } 
-        
-        for (let i = 0; i < $inputPass.length; i++) {
-            if ($inputPass[i].classList.contains('is-invalid')) {
-                changePassowrd.classList.toggle('ds-block')
-                break
-            } 
-        } 
-    
-    
-        $editar.addEventListener('click', ()=>{
-            if ($editar.innerText.toLowerCase() === "editar") {
-                for (let i = 0; i < $inputs.length; i++) {
-                    $editar.style.display = "none"
-                    $inputs[i].disabled = false
-                    $inputs[i].style.color = "black"
-                    $button.style.display = "block"
-                } 
-            } else if($editar.innerText.toLowerCase() === "guardar") {
-                for (let i = 0; i < $inputs.length; i++) {
-                    $inputs[i].disabled = true
-                    $inputs[i].style.color = "rgb(143, 143, 143)"
-                } 
-            }
-            
-        })
-    
-    
-        $btnH5.addEventListener('click', ()=>{
-            changePassowrd.classList.toggle('ds-block')
-        })
-    
-        updateFile.addEventListener('change', ()=>{
-            btnSubmitFile.style.display = "inline"
-        })
-        
-        
-    
-        
-    } 
-     
-  
