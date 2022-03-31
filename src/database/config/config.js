a353230cfc122bef9c2dce6d5389fbe5aedd2ebe
Exequiel65 <marcos97britos@gmail.com>
2022-03-31 18:05:28 -0300

    fixv8

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": process.env.DB_USER2,
+    "password": process.env.DB_PASSWORD2,
+    "database": process.env.DB_DATABASE2,
+    "host": process.env.DB_HOST2,
     "dialect": "mysql"
   },
   "test": {
@@ -15,10 +15,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": process.env.DB_USER2,
+    "password": process.env.DB_PASSWORD2,
+    "database": process.env.DB_DATABASE2,
+    "host": process.env.DB_HOST2,
     "dialect": "mysql"
   }
 }
