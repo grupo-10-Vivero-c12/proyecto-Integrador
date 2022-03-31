8ac3697457f5cc2a3f6504a1cd477b206fc9bb2c
Exequiel65 <marcos97britos@gmail.com>
2022-03-31 19:30:48 -0300

    fixv10

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-    "username": process.env.DB_USER2,
-    "password": process.env.DB_PASSWORD2,
-    "database": process.env.DB_DATABASE2,
-    "host": process.env.DB_HOST2,
+    "username": "root",
+    "password": null,
+    "database": "vivero_timbo",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
