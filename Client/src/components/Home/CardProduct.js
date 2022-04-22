a6de0aec1e9921483aa01f8d04bd763bac084337
Exequiel65 <marcos97britos@gmail.com>
2022-04-21 23:24:38 -0300

    terminado home

--- Client/src/components/Home/CardProduct.js
+++ Client/src/components/Home/CardProduct.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Cardproduct = (props) => {
     console.log(props)
+    const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     return (
         <div className="product-related">
             <div className="image-product">
                 <a href={`/product/detail/${props.id}`} ><img src={`images/products/${props.images}`} alt="" /></a>
             </div>
             <div className="description-product">
-                <a href={`/product/detail/${props.id}`}><p> product.name </p></a>
-                <p>$ toThousand(product.price) </p>
+                <a href={`/product/detail/${props.id}`}><p> {props.name}</p></a>
+                <p>${ toThousand(props.price)} </p>
                 <button className="btn">Comprar</button>
             </div>
         </div>
